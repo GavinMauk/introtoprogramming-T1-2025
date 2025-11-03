67af4e44121639ffccd3acf2fd4242874254562a
GavinMauk <821059@mystma.org>
2025-11-03 15:06:53 -0600

    Update textadventure.py

--- tests/textadventure.py
+++ tests/textadventure.py
@@ -4,27 +4,33 @@ def start():
     print("you have 20 turns")
     a1()
 def a1():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square a1")
+        return
     print("which direction would you like to go.")
     print("1.down")
     print("2.right")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b1()
     elif a==2:
         a2()
-    elif end ==20:
-        print("you finished on square a1")
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         a1()
 def a2():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square a2")
+        return
     print("which direction would you like to go.")
     print("1.down")
     print("2.right")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b2()
     elif a==2:
@@ -32,42 +38,54 @@ def a2():
     elif a==3:
         a1()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         a2()
 def a3():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square a3")
+        return
     print("which direction would you like to go.")
     print("1.down")
     print("2.right")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b3()
     elif a==2:
         a4()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         a3()
 def a4():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square a4")
+        return
     print("which direction would you like to go.")
     print("1.down")
     print("2.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b4()
     elif a==2:
         a3()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         a4()
 def b1():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square b1")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         a1()
     elif a==2:
@@ -75,16 +93,20 @@ def b1():
     elif a==3:
         b2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         b1()
 def b2():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square b2")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         a2()
     elif a==2:
@@ -94,16 +116,20 @@ def b2():
     elif a==4:
         b1()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         b2()
 def b3():
-    end=(int(end)+1)
+    global end
+    end=(end+1)
+    if end ==20:
+        print("you finished on square b3")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         a3()
     elif a==2:
@@ -113,14 +139,19 @@ def b3():
     elif a==4:
         b2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         b3()
 def b4():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square b4")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         a4()
     elif a==2:
@@ -128,14 +159,19 @@ def b4():
     elif a==3:
         b3()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         b4()
 def c1():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square c1")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b1()
     elif a==2:
@@ -143,15 +179,20 @@ def c1():
     elif a==3:
         c2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         c1()
 def c2():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square c2")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b2()
     elif a==2:
@@ -161,15 +202,20 @@ def c2():
     elif a==4:
         c1()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         c2()
 def c3():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square c3")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b3()
     elif a==2:
@@ -179,14 +225,19 @@ def c3():
     elif a==4:
         c2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         c3()
 def c4():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square c4")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         b4()
     elif a==2:
@@ -194,14 +245,19 @@ def c4():
     elif a==3:
         c3()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         c4()
 def d1():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square d1")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         c1()
     elif a==2:
@@ -209,15 +265,20 @@ def d1():
     elif a==3:
         d2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         d1()
 def d2():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square d2")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         c2()
     elif a==2:
@@ -227,15 +288,20 @@ def d2():
     elif a==4:
         d1()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         d2()
 def d3():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square d3")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.right")
     print("4.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         c3()
     elif a==2:
@@ -245,14 +311,19 @@ def d3():
     elif a==4:
         d2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         d3()
 def d4():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square d4")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.down")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         c4()
     elif a==2:
@@ -260,26 +331,36 @@ def d4():
     elif a==3:
         d3()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         d4()
 def e1():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square e1")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.right")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         d1()
     elif a==2:
         e2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         e1()
 def e2():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square e2")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.right")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         d2()
     elif a==2:
@@ -287,14 +368,19 @@ def e2():
     elif a==3:
         e1()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         e2()
 def e3():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square e3")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.right")
     print("3.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         d3()
     elif a==2:
@@ -302,18 +388,23 @@ def e3():
     elif a==3:
         e2()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         e3()
 def e4():
+    global end
+    end= end+1
+    if end ==20:
+        print("you finished on square e4")
+        return
     print("which direction would you like to go.")
     print("1.up")
     print("2.left")
-    a=input(">")
+    a=int(input(">"))
     if a==1:
         d4()
     elif a==2:
         e3()
     else:
-        print("invaled choice. try again.")
+        print("invalid choice. try again.")
         e4()
-    
+start()
